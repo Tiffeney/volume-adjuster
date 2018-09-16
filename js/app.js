6c5376448336b7e1f716459c924c7fa4e292b38d
Tiffeney <tiffeney.rambo@gmail.com>
2018-09-15 21:05:30 -0700

    completed assignment

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
-(function() {
-    // Write code here
-})()
+console.log('JS LOADED')
+
+//Identify the global variables & set them.
+//Add event listener to make sure it works when click 
+//Grab the "0" element 
+//Increment moving to the right 
+
+function volumeAdjuster() {
+    let input = document.getElementById('start')
+    input.addEventListener('change', function () {
+        document.getElementById('volume-level').innerHTML = this.value
+    })
+}
+
+volumeAdjuster()
+
+
+
+
+
+
+
+
